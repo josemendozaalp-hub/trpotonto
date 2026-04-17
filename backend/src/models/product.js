7a51f95c6ef48c20d79da3b88cecd961552d2d86
manuel <jose.mendoza.lp@cbtis258.edu.mx>
2026-04-16 21:05:09 -0400

    ddgrg

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            minlegth: 2,
+        },
+    }
+)

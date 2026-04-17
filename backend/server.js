1a05daf6d828dbcbe3212b9b17037a15567ed784
manuel <jose.mendoza.lp@cbtis258.edu.mx>
2026-04-16 21:02:04 -0400

    Corregir errores tipográficos en server.js y agregar archivo product.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
-import dotend from 'dotenv';
+import dotenv from 'dotenv';
 import app from './src/app.js';
-import connectDB from './src/database/conection.js'
+import connectDB from './src/database/connection.js';
 
-dotend.config();
+dotenv.config();
 
 const PORT = Number(process.env.PORT) || 5000;
 
 const startServer = async () => {
-    try {
-        await connectDB(process.env.MONGODB_URI);
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error('Error starting server:', error.message);
-        process.exit(1);
-    }
-}
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error.message);
+    process.exit(1);
+  }
+};
+
+startServer();

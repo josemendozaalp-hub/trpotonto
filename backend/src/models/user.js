9d70cfcae1d8c961fbbba334f331e2ecefe5b092
manuel <jose.mendoza.lp@cbtis258.edu.mx>
2026-04-16 21:38:25 -0400

    Agregar esquema de usuario con métodos para comparar contraseñas y ocultar la contraseña en la respuesta JSON; crear archivos de controlador, middleware, rutas y utilidades

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -24,10 +24,27 @@ const userSchema = new mongoose.Schema(
         }
     },
     { timestamps: true }
-)
+);
 
 USERSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
         return next();
     }
-})
+
+    this.password = await bcrypt.hash(this.password, 10);
+    return
+});
+
+userSchema.methods.comparePassword = function comparePassword(rawPassword) {
+    return bcrypt.compare(rawPassword, this.password)
+};
+
+userSchema.methods.toJSON = function toJSON() {
+    const userObject = this.toObject();
+    delete userObject.password;
+    return userObject;
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

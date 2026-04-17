d80d61741c381782d54fde82293371e96010f111
manuel <jose.mendoza.lp@cbtis258.edu.mx>
2026-04-16 21:17:21 -0400

    chore: initialize backend with package.json and essential dependencies

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import { create } from "node:domain";
+import { type } from "node:os";
 
 const productSchema = new mongoose.Schema(
     {
@@ -8,5 +10,36 @@ const productSchema = new mongoose.Schema(
             trim: true,
             minlegth: 2,
         },
-    }
-)
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlegth: 500, 
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        stock: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        Image: {
+            type: String,
+            default: "",
+        },
+        createBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            index: true,
+        }
+    },
+    {timestamps: true}
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
